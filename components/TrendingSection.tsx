38626d03718d2d9748f9911c83f449b2e1ecd014
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-26 22:56:49 +0600

    added real data using PaperCard

--- components/TrendingSection.tsx
+++ components/TrendingSection.tsx
@@ -1,16 +1,34 @@
 import { Skeleton } from "@components/ui/skeleton"
+import PaperCard from "./Papers/PaperCard"
+import { ZenodoData } from "@src/apiWrapper/types"
 
-export default function TrendingSection() {
+interface TrendingSectionProps {
+  data: ZenodoData | null
+}
+
+export default function TrendingSection({ data }: TrendingSectionProps) {
   return (
     <div>
       <h1 className="text-md">📈 Trending on TOPSnet</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <TrendCard />
-        <TrendCard />
-        <TrendCard />
-        <TrendCard />
-        <TrendCard />
-        <TrendCard />
+        {data === null ? (
+          Array(6).fill(null).map((_, index) => <TrendCard key={index} />)
+        ) : (
+          Array(6).fill(null).map((_, index) => {
+            return (
+              <PaperCard
+                key = {index}
+                published={data['hits']['hits'][index]['metadata']['publication_date']}
+                resource_type={data['hits']['hits'][index]['metadata']['resource_type']['title']}
+                access={data['hits']['hits'][index]['metadata']['access_right']}
+                title={data['hits']['hits'][index]['metadata']['title']}
+                contributors={data['hits']['hits'][index]['metadata']['creators']}
+                versions={data['hits']['hits'][index]['metadata']['version']}
+              />
+            )
+          }
+          )
+          )}
       </div>
     </div>
   )
@@ -20,9 +38,9 @@ export default function TrendingSection() {
 const TrendCard = () => {
   return (
     <div className="flex items-center space-x-4 my-3">
-      <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
         <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
