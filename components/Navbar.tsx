3fdc49668f496e3e04efef2ad35cf96778ef5147
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-26 22:56:24 +0600

    improved visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC = () => {
   }
 
   return (
-    <nav className="sticky top-0 bg-gray-800 bg-opacity-30 backdrop-blur-lg">
+    <nav className="sticky top-0 bg-gray-800 bg-opacity-30 backdrop-blur-lg z-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
