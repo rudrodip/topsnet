663519bd420ab0803d4f506a48fb9e715f25dcfe
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-27 16:10:51 +0600

    changed for new types

--- components/Papers/PaperCard.tsx
+++ components/Papers/PaperCard.tsx
@@ -1,18 +1,20 @@
-interface Contributors{
-  affiliation: string,
+import Link from "next/link"
+
+interface Contributors {
+  affiliation?: string,
   name: string,
-  orcid: string
+  orcid?: string
 }
 interface PaperCardProps {
+  id: number,
   published: string,
   resource_type: string,
   access: string,
   title: string,
   contributors: Contributors[],
-  versions: string
 }
 
-const PaperCard = ({ published, resource_type, access, title, contributors, versions }: PaperCardProps) => {
+const PaperCard = ({ id, published, resource_type, access, title, contributors }: PaperCardProps) => {
   return (
     <div className="bg-gray-700 bg-opacity-40 backdrop-blur-lg max-w-xl p-4 rounded-lg text-gray-300 hover:bg-gray-900 delay-75 transition-all ease-in-out cursor-pointer m-2 z-10">
       <div className="flex text-sm flex-wrap">
@@ -20,7 +22,9 @@ const PaperCard = ({ published, resource_type, access, title, contributors, vers
         <p className="rounded-sm bg-gray-500 p-1 mr-1">{resource_type.toUpperCase()}</p>
         <p className="rounded-sm bg-green-600 p-1 mr-1">{access.toUpperCase()}</p>
       </div>
-      <h1 className="hover:text-white text-lg font-bold my-2">{title}</h1>
+      <Link href={`/record/${id}`}>
+        <h1 className="hover:text-white text-lg font-bold my-2">{title}</h1>
+      </Link>
       <p>Contributors</p>
       <div className="flex flex-wrap my-2">
         {contributors.map(elem => {
@@ -29,7 +33,6 @@ const PaperCard = ({ published, resource_type, access, title, contributors, vers
           )
         })}
       </div>
-      <p className="text-sm">Version {versions}</p>
     </div>
   );
 };
