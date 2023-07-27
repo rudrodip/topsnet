50948b816195c6e6a9c1f3e5cd3a240fffa8aba1
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-27 16:11:15 +0600

    changed for the types

--- components/PaperSection.tsx
+++ components/PaperSection.tsx
@@ -37,12 +37,12 @@ export default function PaperSection({ data }: PaperSectionProps) {
               return (
                 <PaperCard
                   key={index}
+                  id={data['hits']['hits'][index]['id']}
                   published={data['hits']['hits'][index]['metadata']['publication_date']}
                   resource_type={data['hits']['hits'][index]['metadata']['resource_type']['title']}
                   access={data['hits']['hits'][index]['metadata']['access_right']}
                   title={data['hits']['hits'][index]['metadata']['title']}
                   contributors={data['hits']['hits'][index]['metadata']['creators']}
-                  versions={data['hits']['hits'][index]['metadata']['version']}
                 />
               )
             }
