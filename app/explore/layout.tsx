fb47a9551dbdc99fe569318be6b1daa1c362d91f
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-28 18:40:37 +0600

    layout for explorer page

---
+++ app/explore/layout.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Navbar from "@components/Navbar";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <Navbar />
+        {children}
+    </>
+  )
+}
