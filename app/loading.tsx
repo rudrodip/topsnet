8202c3c63ac7eb787f76039a8727f2d8b0182ffd
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-28 18:40:56 +0600

    added loading page

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import LoadSpinner from "@components/LoadSpinner"
+
+export default function Loading() {
+  
+  return <div className="flex justify-center align-middle items-center min-h-screen">
+    <LoadSpinner />
+  </div>
+}
