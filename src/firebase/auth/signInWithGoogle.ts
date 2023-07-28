7324de31a0ba33528c311f95b163813f834f024e
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-28 18:37:49 +0600

    added new user bool

--- src/firebase/auth/signInWithGoogle.ts
+++ src/firebase/auth/signInWithGoogle.ts
@@ -1,15 +1,17 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, UserCredential, getAdditionalUserInfo } from "firebase/auth";
 import { auth } from "../config";
 const provider = new GoogleAuthProvider();
 
 const signInWithGoogle = async () => {
-  signInWithPopup(auth, provider)
-  .then((result) => {
+  try {
+    const result: UserCredential = await signInWithPopup(auth, provider);
     const user = result.user;
-    return { user }
-  }).catch((error) => {
-    return { error }
-  });
+    const additionalUserInfo = getAdditionalUserInfo(result);
+    const isNewUser = additionalUserInfo?.isNewUser || false;
+    return { user, isNewUser };
+  } catch (error) {
+    return { error };
+  }
 };
 
 export default signInWithGoogle
