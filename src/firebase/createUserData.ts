8f8718d507d4a32970c8b0afd5b1067b891269a0
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-28 22:06:46 +0600

    updated update func

--- src/firebase/createUserData.ts
+++ src/firebase/createUserData.ts
@@ -1,6 +1,6 @@
 import { User } from "firebase/auth";
 import { db } from "./config";
-import { doc, setDoc, updateDoc } from "firebase/firestore";
+import { doc, setDoc, updateDoc, UpdateData, DocumentData } from "firebase/firestore";
 
 async function createUserData(user: User | null, data: Partial<JSON>) {
   if (!user) return;
@@ -12,12 +12,12 @@ async function createUserData(user: User | null, data: Partial<JSON>) {
   }
 }
 
-async function updateUserData(user: User | null, dataToUpdate: Partial<JSON>) {
+async function updateUserData(user: User | null, dataToUpdate: UpdateData<DocumentData>) {
   if (!user) return;
   
   try {
     const userRef = doc(db, `user/${user.uid}`);
-    await setDoc(userRef, dataToUpdate, { merge: true });
+    await updateDoc(userRef, dataToUpdate);
     console.log("User data updated successfully!");
   } catch (error) {
     console.error("Error updating user data:", error);
