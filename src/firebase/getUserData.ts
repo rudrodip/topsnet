82db106a3a079fa11d6e5b944e59e1bab5fcb8f2
rudrodip <official.rudrodipsarker@gmail.com>
2023-07-29 11:33:59 +0600

    new function for getting user data

---
+++ src/firebase/getUserData.ts
@@ -0,0 +1,21 @@
+import { db } from "./config";
+import { doc, getDoc } from 'firebase/firestore'
+import { User } from "firebase/auth";
+
+interface UserData{
+  liked: number[],
+  pinned: number[],
+  recommended: number[]
+}
+
+async function getUserData(user: User | null): Promise<UserData | null>{
+  const ref = doc(db, `user/${user?.uid}`)
+  const snapShot = await getDoc(ref)
+  if (snapShot.exists()){
+    return snapShot.data() as UserData
+  } else {
+    return null
+  }
+}
+
+export { getUserData }
