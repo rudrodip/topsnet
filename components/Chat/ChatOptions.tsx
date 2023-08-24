e59cfda04223876c5d31aa1cec062b23dbfe7f92
rudrodip <official.rudrodipsarker@gmail.com>
2023-08-24 20:01:32 +0600

    updated chatoptions props and response handler

--- components/Chat/ChatOptions.tsx
+++ components/Chat/ChatOptions.tsx
@@ -2,8 +2,12 @@ import React, { useState, useEffect, useRef, FormEvent } from 'react';
 import { Text, X } from 'lucide-react';
 import { useChatContext } from '@context/ChatContext';
 
-const ChatWindow: React.FC = () => {
-  const { messages, addMessage } = useChatContext();
+interface ChatOptionsProps {
+  recordDesc: string | undefined
+}
+
+const ChatWindow = ({ recordDesc }: ChatOptionsProps) => {
+  const { messages, addMessage, error, loading } = useChatContext();
   const [showChat, setShowChat] = useState(false);
   const [newMessage, setNewMessage] = useState('');
   const chatWindowRef = useRef<HTMLDivElement>(null);
@@ -20,6 +24,13 @@ const ChatWindow: React.FC = () => {
     e.preventDefault();
     setNewMessage(e.target.value);
   };
+
+  const handleAnalyzePaper = () => {
+    if (recordDesc){
+      let prompt = `Here's the description of the paper: ${recordDesc}. Analyze it and give me insights`
+      addMessage(prompt);
+    }
+  }
 
   const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -39,31 +50,54 @@ const ChatWindow: React.FC = () => {
 
   return (
     <div className="fixed bottom-8 right-8">
-      <button
-        className="my-5 bg-secondary text-white rounded-full w-12 h-12 flex justify-center items-center"
-        onClick={handleToggleChat}
-      >
-        {showChat ? <X /> : <Text />}
-      </button>
+      <div className='flex items-center'>
+        <button
+          className="my-5 bg-secondary text-white rounded-full w-12 h-12 flex justify-center items-center"
+          onClick={handleToggleChat}
+        >
+          {showChat ? <X /> : <Text />}
+        </button>
+        {showChat ?
+          <button
+            className="bg-cyan-500 text-white px-3 py-1 ml-3 rounded-md"
+            onClick={handleAnalyzePaper}
+          >
+            Analyze Paper
+          </button>
+
+          : ''
+        }
+      </div>
+
       {showChat && (
         <div
-          className={`chat-window bg-gray-800 border rounded-lg p-4 w-96 h-96 text-white overflow-y-auto`}
+          className={`chat-window bg-gray-800 border rounded-lg p-4 w-80 md:w-96 h-96 text-white overflow-y-auto`}
           ref={chatWindowRef}
         >
           <div className={`flex-1 mb-2`}>
             {messages.map((message, index) => (
               <div
                 key={message.id}
-                className={`mb-2 ${message.role == 'ai' ? 'ml-8' : 'mr-8'} ${index === messages.length - 1 ? 'mt-2' : ''
+                className={`mb-2 ${message.role == 'assistant' ? 'ml-8' : 'mr-8'} ${index === messages.length - 1 ? 'mt-2' : ''
                   }`}
               >
                 <div
-                  className={`rounded-lg p-3 ${message.role == 'ai' ? 'bg-gray-700' : 'bg-blue-500'}`}
+                  className={`rounded-lg p-3 ${message.role == 'assistant' ? 'bg-gray-700' : 'bg-blue-500'}`}
                 >
                   <p>{message.content}</p>
                 </div>
               </div>
             ))}
+            {loading ? <div
+              className='rounded-lg p-3 bg-gray-700'
+            >
+              <p>Loading...</p>
+            </div> : ''}
+            {error ? <div
+              className='rounded-lg p-3 bg-red-500'
+            >
+              <p>{error}</p>
+            </div> : ''}
           </div>
           <form className="flex mt-2 flex-wrap justify-between" onSubmit={handleSendMessage}>
             <input
@@ -73,10 +107,11 @@ const ChatWindow: React.FC = () => {
               ref={inputRef}
               className="rounded-md border p-2 mr-2 bg-gray-700 text-white outline-none"
               placeholder="Type your message..."
+              maxLength={50}
             />
             <button
               type='submit'
-              className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
             >
               Send
             </button>
